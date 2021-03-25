dcf59171bfd1e0fdb503b7864c683c08f71a40f5
celso341 <celso341@gmail.com>
2021-03-25 18:28:03 -0300

    Update

---
+++ backend/src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
+import "./database/connection";
+
+import routes from './routes';
+import errroHandler from './errors/handler';
+
+const app =express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errroHandler);
+
+
+
+
+app.listen(3333);
+console.log("Server rodando na port 3333.");
+
